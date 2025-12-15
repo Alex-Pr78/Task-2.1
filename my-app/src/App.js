b57bcae8b7439b68bfbe496832943eb7b285c6f1
Алексей <METEOP2024@yandex.ru>
2025-12-15 14:12:24 +0300

    displaying the content of the active step in the block

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -31,37 +31,20 @@ export const App = () => {
 				<h1>Инструкция по готовке пельменей</h1>
 				<div className={styles.steps}>
 					<div className={styles['steps-content']}>
-						{/* Для получения активного контента использйте steps и activeIndex */}
-						Контент соответственный шагу. Сейчас активен шаг 3
+						<h2>{currentStep.title}</h2>
+						<p>{currentStep.content}</p>
 					</div>
 					<ul className={styles['steps-list']}>
-						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
-						<li className={styles['steps-item'] + ' ' + styles.done}>
-							{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
-							<button className={styles['steps-item-button']}>1</button>
-							{/* При клике на кнопку установка выбранного шага в качестве активного */}
-							Шаг 1
-						</li>
-						<li className={styles['steps-item'] + ' ' + styles.done}>
-							<button className={styles['steps-item-button']}>2</button>
-							Шаг 2
-						</li>
-						<li
-							className={
-								styles['steps-item'] +
-								' ' +
-								styles.done +
-								' ' +
-								styles.active
-							}
-						>
-							<button className={styles['steps-item-button']}>3</button>
-							Шаг 3
-						</li>
-						<li className={styles['steps-item']}>
-							<button className={styles['steps-item-button']}>4</button>
-							Шаг 4
-						</li>
+						{steps.map((step, index) => {
+							const isDone = index < activeIndex;
+
+							return (
+								<li key={step.id} className={`${styles['steps-item']} ${isDone ? styles.done : ''}`}>
+									<button className={styles['steps-item-button']}>{index + 1}</button>
+									{step.title}
+								</li>
+							)
+						})}
 					</ul>
 					<div className={styles['buttons-container']}>
 						<button className={styles.button} onClick={handlerPref} disabled={isFirstStep}>Назад</button>
